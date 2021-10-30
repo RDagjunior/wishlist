83719533526a0990c31ec71a225a2cb58820015d
agjunior <amilton.rodrigues@rdsaude.com.br>
2021-10-30 10:43:07 -0300

    fix: remove unnecessary classes

--- src/main/java/br/com/luizalabs/wishlist/domain/Wishlist.java
+++ src/main/java/br/com/luizalabs/wishlist/domain/Wishlist.java
@@ -1,15 +1,12 @@
 package br.com.luizalabs.wishlist.domain;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -23,9 +20,4 @@ public class Wishlist {
     private ObjectId id;
     private String customerId;
     private List<String> products;
-
-    @CreatedDate
-    private LocalDateTime creationDate;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
 }
