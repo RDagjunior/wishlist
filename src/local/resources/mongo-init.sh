39ecd1c30ca9cb12c3d0d073cadd66ab2715f411
agjunior <amilton.rodrigues@rdsaude.com.br>
2021-10-30 10:41:30 -0300

    fix: remove unnecessary classes

--- src/local/resources/mongo-init.sh
+++
@@ -1,1 +0,0 @@
-mongo --eval "db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD'); db = db.getSiblingDB('wishlist'); db.createUser({ user: 'wishlist-user', pwd: 'wishlist-password', roles: [{ role: 'readWrite', db: 'wishlist' }, { role: 'dbAdmin', db: 'wishlist'}] });"
